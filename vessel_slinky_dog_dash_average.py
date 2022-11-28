b0b6200e2b7dca6054e790bd8c0ff81a38d4adee
smjohnsonShipyard <104784108+smjohnsonShipyard@users.noreply.github.com>
2022-11-28 14:40:00 -0500

    Update vessel_slinky_dog_dash_average.py

--- vessel_slinky_dog_dash_average.py
+++ vessel_slinky_dog_dash_average.py
@@ -4,7 +4,7 @@ import pandas as pd
 dataframe = pd.read_csv('slinky_dog_dash.csv')
 print('Slinky Dog Dash File Read')
 clean_dataframe = dataframe[dataframe.SACTMIN.notnull()]
-clean_dataframe.drop(["datetime", "SPOSTMIN"], axis = 1, inplace=True)
+clean_dataframe.drop(["DATETIME", "SPOSTMIN"], axis = 1, inplace=True)
 daily_average = clean_dataframe.groupby("date")["SACTMIN"].mean()
 print('Daily Average Found')
 daily_average_df = daily_average.to_frame()
