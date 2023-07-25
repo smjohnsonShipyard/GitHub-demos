3129301db487089d3806599ac219eb9e8738ff34
smjohnsonShipyard <104784108+smjohnsonShipyard@users.noreply.github.com>
2023-07-25 12:46:56 -0400

    Create download_slinky_dog.py

---
+++ download_slinky_dog.py
@@ -0,0 +1,13 @@
+import requests
+
+url = 'https://cdn.touringplans.com/datasets/slinky_dog.csv'
+response = requests.get(url)
+
+# ensure we got a valid response
+if response.status_code == 200:
+    # open in binary mode
+    with open('slinky_dog.csv', 'wb') as file:
+        # write to file
+        file.write(response.content)
+else:
+    print(f"Failed to get URL. Status code: {response.status_code}")
